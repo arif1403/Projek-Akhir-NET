ddd375b0a93858d3bd5fe49f39e2cd537e5658a1
meira nur <meira nur@DESKTOP-0U21RA2>
2022-01-12 17:49:10 +0700

    di commit oleh moh faiq ainunnuza: menambahkan Form Stock

---
+++ stock management.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace projekakhir
+{
+    partial class stock_management
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbqunatity = new System.Windows.Forms.TextBox();
+            this.cmbitem = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbdescrption = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnsearch = new System.Windows.Forms.Button();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.Stock = new System.Windows.Forms.ListView();
+            this.btndasbord = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Item";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Qunatity";
+            // 
+            // tbqunatity
+            // 
+            this.tbqunatity.Location = new System.Drawing.Point(115, 65);
+            this.tbqunatity.Name = "tbqunatity";
+            this.tbqunatity.Size = new System.Drawing.Size(128, 20);
+            this.tbqunatity.TabIndex = 2;
+            // 
+            // cmbitem
+            // 
+            this.cmbitem.FormattingEnabled = true;
+            this.cmbitem.Location = new System.Drawing.Point(115, 30);
+            this.cmbitem.Name = "cmbitem";
+            this.cmbitem.Size = new System.Drawing.Size(128, 21);
+            this.cmbitem.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(328, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Description";
+            // 
+            // tbdescrption
+            // 
+            this.tbdescrption.Location = new System.Drawing.Point(404, 65);
+            this.tbdescrption.Name = "tbdescrption";
+            this.tbdescrption.Size = new System.Drawing.Size(128, 20);
+            this.tbdescrption.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbdescrption);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cmbitem);
+            this.groupBox1.Controls.Add(this.tbqunatity);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(24, 46);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(559, 111);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Stock Entry Form";
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(48, 194);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(76, 29);
+            this.btnsave.TabIndex = 7;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Location = new System.Drawing.Point(155, 194);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(76, 29);
+            this.btnupdate.TabIndex = 8;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(253, 194);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(76, 29);
+            this.btndelete.TabIndex = 9;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            // 
+            // btnsearch
+            // 
+            this.btnsearch.Location = new System.Drawing.Point(352, 194);
+            this.btnsearch.Name = "btnsearch";
+            this.btnsearch.Size = new System.Drawing.Size(76, 29);
+            this.btnsearch.TabIndex = 10;
+            this.btnsearch.Text = "Search";
+            this.btnsearch.UseVisualStyleBackColor = true;
+            // 
+            // btnexit
+            // 
+            this.btnexit.Location = new System.Drawing.Point(453, 194);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(76, 29);
+            this.btnexit.TabIndex = 11;
+            this.btnexit.Text = "Exit";
+            this.btnexit.UseVisualStyleBackColor = true;
+            // 
+            // Stock
+            // 
+            this.Stock.HideSelection = false;
+            this.Stock.Location = new System.Drawing.Point(16, 253);
+            this.Stock.Name = "Stock";
+            this.Stock.Size = new System.Drawing.Size(566, 164);
+            this.Stock.TabIndex = 12;
+            this.Stock.UseCompatibleStateImageBehavior = false;
+            // 
+            // btndasbord
+            // 
+            this.btndasbord.Location = new System.Drawing.Point(550, 194);
+            this.btndasbord.Name = "btndasbord";
+            this.btndasbord.Size = new System.Drawing.Size(76, 29);
+            this.btndasbord.TabIndex = 13;
+            this.btndasbord.Text = "Dasboard";
+            this.btndasbord.UseVisualStyleBackColor = true;
+            // 
+            // stock_management
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 429);
+            this.Controls.Add(this.btndasbord);
+            this.Controls.Add(this.Stock);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.btnsearch);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "stock_management";
+            this.Text = "stock_management";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbqunatity;
+        private System.Windows.Forms.ComboBox cmbitem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbdescrption;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnsearch;
+        private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.ListView Stock;
+        private System.Windows.Forms.Button btndasbord;
+    }
+}
